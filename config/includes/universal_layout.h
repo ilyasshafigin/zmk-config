63b1017b5bfb6b9f2073f646132a03c544d3379a
Ilyas Shafigin <dev@ilyasshafigin.ru>
2025-04-05 23:26:41 +0300

    Обновление keymap

--- config/includes/universal_layout.h
+++ config/includes/universal_layout.h
@@ -9,3 +9,24 @@
 #define UL_FSLH BSLH        // -> '/'
 #define UL_PIPE LS(BSLH)    // -> '|'
 #define UL_DDV LS(LA(BSLH)) // -> '¦' - Double Dash Vertical
+
+#define UL_N1 LS(N1)        // -> 1
+#define UL_N2 LS(N2)        // -> 2
+#define UL_N3 LS(N3)        // -> 3
+#define UL_N4 LS(N4)        // -> 4
+#define UL_N5 LS(N5)        // -> 5
+#define UL_N6 LS(N6)        // -> 6
+#define UL_N7 LS(N7)        // -> 7
+#define UL_N8 LS(N8)        // -> 8
+#define UL_N9 LS(N9)        // -> 9
+#define UL_N0 LS(N0)        // -> 0
+#define UL_SN1 N1           // -> !
+#define UL_SN2 N2           // -> @
+#define UL_SN3 N3           // -> #
+#define UL_SN4 N4           // -> $
+#define UL_SN5 N5           // -> %
+#define UL_SN6 N6           // -> ^
+#define UL_SN7 N7           // -> ?
+#define UL_SN8 N8           // -> *
+#define UL_SN9 N9           // -> (
+#define UL_SN0 N0           // -> )
