b15bf9bd137a281bf946e3e15d4179eda8f5c274
Ilyas Shafigin <dev@ilyasshafigin.ru>
2025-04-08 19:19:44 +0300

    Обновление keymap

--- config/includes/universal_layout.h
+++ config/includes/universal_layout.h
@@ -1,32 +1,33 @@
-#define UL_TILDE SEMI       // -> ~/ж
-#define UL_AMPS FSLH        // -> &/ю
+#define UL_TILDE    SEMI            // -> ~/ж
+#define UL_AMPS     FSLH            // -> &/ю
 
-#define UL_SQT GRAVE        // -> '''
-#define UL_DQT LS(GRAVE)    // -> '"'
-#define UL_GRAVE LA(GRAVE)  // -> '`'
+#define UL_SQT      GRAVE           // -> '''
+#define UL_DQT      LS(GRAVE)       // -> '"'
+#define UL_GRAVE    LA(GRAVE)       // -> '`'
 
-#define UL_BSLH LA(BSLH)    // -> '\'
-#define UL_FSLH BSLH        // -> '/'
-#define UL_PIPE LS(BSLH)    // -> '|'
-#define UL_DDV LS(LA(BSLH)) // -> '¦' - Double Dash Vertical
+#define UL_BSLH     LA(BSLH)        // -> '\'
+#define UL_FSLH     BSLH            // -> '/'
+#define UL_PIPE     LS(BSLH)        // -> '|'
+#define UL_DDV      LS(LA(BSLH))    // -> '¦' - Double Dash Vertical
 
-#define UL_N1 LS(N1)        // -> 1
-#define UL_N2 LS(N2)        // -> 2
-#define UL_N3 LS(N3)        // -> 3
-#define UL_N4 LS(N4)        // -> 4
-#define UL_N5 LS(N5)        // -> 5
-#define UL_N6 LS(N6)        // -> 6
-#define UL_N7 LS(N7)        // -> 7
-#define UL_N8 LS(N8)        // -> 8
-#define UL_N9 LS(N9)        // -> 9
-#define UL_N0 LS(N0)        // -> 0
-#define UL_SN1 N1           // -> !
-#define UL_SN2 N2           // -> @
-#define UL_SN3 N3           // -> #
-#define UL_SN4 N4           // -> $
-#define UL_SN5 N5           // -> %
-#define UL_SN6 N6           // -> ^
-#define UL_SN7 N7           // -> ?
-#define UL_SN8 N8           // -> *
-#define UL_SN9 N9           // -> (
-#define UL_SN0 N0           // -> )
+#define UL_N1       LS(N1)  // -> 1
+#define UL_N2       LS(N2)  // -> 2
+#define UL_N3       LS(N3)  // -> 3
+#define UL_N4       LS(N4)  // -> 4
+#define UL_N5       LS(N5)  // -> 5
+#define UL_N6       LS(N6)  // -> 6
+#define UL_N7       LS(N7)  // -> 7
+#define UL_N8       LS(N8)  // -> 8
+#define UL_N9       LS(N9)  // -> 9
+#define UL_N0       LS(N0)  // -> 0
+
+#define UL_EXCL     N1      // -> !
+#define UL_AT       N2      // -> @
+#define UL_HASH     N3      // -> #
+#define UL_DLLR     N4      // -> $
+#define UL_PRCNT    N5      // -> %
+#define UL_CARET    N6      // -> ^
+#define UL_QSTM     N7      // -> ?
+#define UL_ASTRK    N8      // -> *
+#define UL_LPAR     N9      // -> (
+#define UL_RPAR     N0      // -> )
