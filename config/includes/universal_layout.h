9f4f796f399b907fa432846c29f6cc9b9aef9fe2
Ilyas Shafigin <dev@ilyasshafigin.ru>
2025-04-09 10:49:08 +0300

    Обновление keymap

--- config/includes/universal_layout.h
+++ config/includes/universal_layout.h
@@ -1,5 +1,16 @@
-#define UL_TILDE    SEMI            // -> ~/ж
-#define UL_AMPS     FSLH            // -> &/ю
+// На основе раскладки Universal Layout Ortho
+
+// O|Щ -> tap O|З
+// P|З -> tap P|Х
+
+#define UL_COMMA    LBKT            // -> tap: ',', shifted: ';'
+#define UL_DOT      RBKT            // -> tap: '.', shifted: ':'
+#define UL_TILDE    SEMI            // -> tap: ~|ж, shifted: ≈|Ж
+#define UL_AMPS     COMMA           // -> tap: &|б, shifted: &|Б
+#define UL_LBKT     DOT             // -> tap: [|ю, shifted: {|Ю, alted: [, shifted+alted: {
+#define UL_RBKT     FSLH            // -> tap: ]|э, shifted: }|Э, alted: ], shifted+alted: }
+#define UL_LBRC     LS(DOT)         // -> {|Ю, alted: {
+#define UL_RBRC     LS(FSLH)        // -> }|Э, alted: }
 
 #define UL_SQT      GRAVE           // -> '''
 #define UL_DQT      LS(GRAVE)       // -> '"'
@@ -8,7 +19,7 @@
 #define UL_BSLH     LA(BSLH)        // -> '\'
 #define UL_FSLH     BSLH            // -> '/'
 #define UL_PIPE     LS(BSLH)        // -> '|'
-#define UL_DDV      LS(LA(BSLH))    // -> '¦' - Double Dash Vertical
+#define UL_DPIPE    LS(LA(BSLH))    // -> '¦'
 
 #define UL_N1       LS(N1)  // -> 1
 #define UL_N2       LS(N2)  // -> 2
@@ -21,13 +32,13 @@
 #define UL_N9       LS(N9)  // -> 9
 #define UL_N0       LS(N0)  // -> 0
 
-#define UL_EXCL     N1      // -> !
-#define UL_AT       N2      // -> @
-#define UL_HASH     N3      // -> #
-#define UL_DLLR     N4      // -> $
-#define UL_PRCNT    N5      // -> %
-#define UL_CARET    N6      // -> ^
-#define UL_QSTM     N7      // -> ?
-#define UL_ASTRK    N8      // -> *
-#define UL_LPAR     N9      // -> (
-#define UL_RPAR     N0      // -> )
+#define UL_EXCL     N1      // -> !, shifted: 1
+#define UL_AT       N2      // -> @, shifted: 2
+#define UL_HASH     N3      // -> #, shifted: 3
+#define UL_DLLR     N4      // -> $, shifted: 4
+#define UL_PRCNT    N5      // -> %, shifted: 5
+#define UL_CARET    N6      // -> ^, shifted: 6
+#define UL_QSTM     N7      // -> ?, shifted: 7
+#define UL_ASTRK    N8      // -> *, shifted: 8
+#define UL_LPAR     N9      // -> (, shifted: 9
+#define UL_RPAR     N0      // -> ), shifted: 0
