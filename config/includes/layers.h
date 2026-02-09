0d314792bb401f42298df3c56db9ec9f3cb65a80
Ilyas Shafigin <dev@ilyasshafigin.ru>
2026-02-09 21:41:40 +0300

    update config

--- config/includes/layers.h
+++ config/includes/layers.h
@@ -6,8 +6,8 @@
 #define GAM 4
 #define GFN 5
 #define FN  6
-#define MSE 7
-#define ADJ 8
+#define ADJ 7
+#define MSE 8
 #define SLOW 9  // for trackball
 #define SCRL 10 // for trackball
 #define CARE 11 // for trackball
