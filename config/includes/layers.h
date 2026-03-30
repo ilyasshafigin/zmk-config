8da5117ff6f390318d653ccb4fda3bc58a8228dd
Ilyas Shafigin <dev@ilyasshafigin.ru>
2026-03-30 21:39:10 +0300

    update keymap

--- config/includes/layers.h
+++ config/includes/layers.h
@@ -3,10 +3,10 @@
 #define SYM 1
 #define NAV 2
 #define NUM 3
-#define GAM 4
-#define GFN 5
-#define ALT 6
-#define CMD 7
+#define ALT 4
+#define CMD 5
+#define GAM 6
+#define GFN 7
 #define MSE 8   // for trackball
 #define SLOW 9  // for trackball
 #define SCRL 10 // for trackball
